a999d8ea8739247f40cf2853512e71c8b81bccdb
Grenish rai <mrcoder2033d@gmail.com>
2026-05-25 20:48:38 +0530

    style(cli): adjust Header layout to vertical column with gap

--- packages/cli/src/components/header.tsx
+++ packages/cli/src/components/header.tsx
@@ -1,9 +1,11 @@
 export function Header() {
   return (
     <box justifyContent="center" alignItems="center">
-      <box flexDirection="row" justifyContent="center" gap={0.5} alignItems="center">
-        <ascii-font font="tiny" text="bug" color={"gray"} />
-        <ascii-font font="tiny" text="ly" />
+      <box flexDirection="column" justifyContent="center" gap={1} alignItems="center">
+        <box flexDirection="row" justifyContent="center" gap={1}>
+          <ascii-font font="tiny" text="bug" color="gray" />
+          <ascii-font font="tiny" text="ly" />
+        </box>
       </box>
     </box>
   )
