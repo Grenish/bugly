5f85b3bfc7eb49edd8ffe5c8f5da1992a6fe19a8
Grenish rai <mrcoder2033d@gmail.com>
2026-05-29 03:34:42 +0530

    style(cli): apply theme colors to header logo

--- packages/cli/src/components/header.tsx
+++ packages/cli/src/components/header.tsx
@@ -1,10 +1,13 @@
+import { useTheme } from "../providers/theme";
+
 export function Header() {
+  const { colors } = useTheme();
   return (
     <box justifyContent="center" alignItems="center">
       <box flexDirection="column" justifyContent="center" gap={1} alignItems="center">
         <box flexDirection="row" justifyContent="center" gap={1}>
-          <ascii-font font="tiny" text="bug" color="gray" />
-          <ascii-font font="tiny" text="ly" />
+          <ascii-font font="tiny" text="bug" color={colors.dimSeparator} />
+          <ascii-font font="tiny" text="ly" color={colors.primary} />
         </box>
       </box>
     </box>
