eba5fb630dd727c29e7d9ef6ffb3eeb597249748
Grenish rai <mrcoder2033d@gmail.com>
2026-06-03 03:30:50 +0530

    feat(server): add env loader to read .env from monorepo root

---
+++ packages/server/src/lib/env.ts
@@ -0,0 +1,8 @@
+import { config } from "dotenv";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const rootEnvPath = resolve(__dirname, "../../../../.env");
+
+config({ path: rootEnvPath });
